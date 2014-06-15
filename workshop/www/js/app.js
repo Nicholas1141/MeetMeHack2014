189689bab01082cd6531390de8115c3a5b42f3fc
Ilonze <linda.ilonze@gmail.com>
2014-06-15 10:47:05 +0100

    Merge branch 'master' of https://github.com/laureandrieux/MeetMeHack2014
    Conflicts:
    .idea/workspace.xml
    workshop/www/js/controllers/EventListController.js

--- workshop/www/js/app.js
+++ workshop/www/js/app.js
@@ -14,7 +14,7 @@ angular.module('peopleTracker', [
     "mobile-angular-ui"])
     .config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/createPerson', {templateUrl: './partials/createPerson.html', controller: 'AddPersonCtrl'});
-    $routeProvider.when('/events/:userName', {templateUrl: './partials/eventlist.html', controller: 'EventListController'});
+    $routeProvider.when('/events', {templateUrl: './partials/eventlist.html', controller: 'EventListController'});
     $routeProvider.when('/event/:eventId', {templateUrl: './partials/eventDetail.html', controller: 'MapCtrl'});
     $routeProvider.when('/createEvent', {templateUrl: './partials/createEvent.html', controller: 'AddEventCtrl'});
  //   $routeProvider.when('/createPerson/:eventId', {templateUrl: './partials/createPerson.html', controller: 'AddPersonCtrl'});
