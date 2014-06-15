de9c385fc0989e26c73b19fa2d5557db7509210f
Ilonze <linda.ilonze@gmail.com>
2014-06-15 04:07:36 +0100

    adding events

--- workshop/www/js/controllers/EventListController.js
+++ workshop/www/js/controllers/EventListController.js
@@ -27,25 +27,34 @@ angular.module('peopleTracker.controllers', [])
                 subscribe_key : 'sub-c-a709d546-f3cd-11e3-928e-02ee2ddab7fe'
             });
 
-        $scope.publish = function(eventChannel) {
-            PubNub.ngPublish({
-                channel: eventChannel ,
-                message: {
-                    "subscriber" : "subscriberA",
-                    "latitude": 64.546456456,
-                    "longitude": -133.3535345
-                }
-            });
+        var publish = function() {
+            navigator.geolocation.watchPosition(geolocationSuccess, geolocationError);
         };
-        $scope.subscribe = function(eventChannel) {
-            PubNub.ngSubscribe({ channel: eventChannel })
-            $rootScope.$on(PubNub.ngMsgEv(eventChannel), function (event, payload) {
-                alert( payload.message);
+
+          var geolocationSuccess = function(position){
+
+              PubNub.ngPublish({
+                  channel: $scope.selectedEvent ,
+                  message: {
+                      "subscriber" : "subscriberA",
+                      "latitude": position.coords.latitude,
+                      "longitude": position.coords.longitude
+                  }
+              });
+      }
+
+
+            var geolocationError = function(error){
+                alert(error);
+          }
+        $scope.subscribe = function(eventName) {
+            PubNub.ngSubscribe({ channel: eventName})
+            $rootScope.$on(PubNub.ngMsgEv(eventName), function (event, payload) {
+                alert( payload.message.subscriber + payload.message.latitude + payload.message.longitude);
             })
-            $rootScope.$on(PubNub.ngPrsEv(eventChannel), function (event, payload) {
-                // payload contains message, channel, env...
-                alert( PubNub.ngListPresence(eventChannel) + "presence one");
-            })
+            $scope.selectedEvent = eventName;
+            publish();
+         //  setInterval( $scope.publish(), 2000);
         }
 
          Events.get()
@@ -54,35 +63,35 @@ angular.module('peopleTracker.controllers', [])
                 })
 }])
     .controller('MapCtrl', function($scope) {
-    
-    $scope.lat = 46.87916;
-    
-    $scope.markersMap = {};
-    
-    $scope.mapPin = 'No pin set yet';
-    
-    $scope.marker = { id: 1, lat: 20, long: 20 };
-    
-    $scope.onClick = function () {
-                
-        $scope.lat += 0.1;
-        $scope.marker = { id: 1, lat: $scope.lat, long: -3.32910 };
-    };
-    
-    $scope.addMarker = function (id, pos) {
-        
-        alert(pos);
-        
-        var map = new google.maps.Map(document.getElementById(attrs.id), myOptions);
-        
-        var myLatlng = new google.maps.LatLng(pos.lat, pos.lng);
-        
-        var marker = new google.maps.Marker({
-                                            position: myLatlng,
-                                            map: map,
-                                            title: "Hello World!"
-                                            });
-        
-        scope.markersMap[id] = marker;
-    };
-                });
+
+        $scope.lat = 46.87916;
+
+        $scope.markersMap = {};
+
+        $scope.mapPin = 'No pin set yet';
+
+        $scope.marker = { id: 1, lat: 20, long: 20 };
+
+        $scope.onClick = function () {
+
+            $scope.lat += 0.1;
+            $scope.marker = { id: 1, lat: $scope.lat, long: -3.32910 };
+        };
+
+        $scope.addMarker = function (id, pos) {
+
+            alert(pos);
+
+            var map = new google.maps.Map(document.getElementById(attrs.id), myOptions);
+
+            var myLatlng = new google.maps.LatLng(pos.lat, pos.lng);
+
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                title: "Hello World!"
+            });
+
+            scope.markersMap[id] = marker;
+        };
+    });
