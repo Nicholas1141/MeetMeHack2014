0c2b45861f6b7679e3c4ba05f247a9e5ac3dae3e
Ilonze <linda.ilonze@gmail.com>
2014-06-15 10:49:47 +0100

    adding multiple person

--- workshop/www/js/controllers/EventListController.js
+++ workshop/www/js/controllers/EventListController.js
@@ -4,17 +4,17 @@
 
 angular.module('peopleTracker.controllers', [])
     .controller('EventListController',['$scope', '$rootScope', '$window',
-                                       '$location', 'Events','PubNub', function ($scope, $rootScope, $window, $location,Events,PubNub) {
-        $scope.slide = '';
-        $rootScope.back = function() {
-            $scope.slide = 'slide-right';
-            $window.history.back();
-        }
-        $rootScope.go = function(path){
-            console.log(path);
-            $scope.slide = 'slide-left';
-            $location.url(path);
-        }
+        '$location', 'Events','PubNub', function ($scope, $rootScope, $window, $location,Events,PubNub) {
+            $scope.slide = '';
+            $rootScope.back = function() {
+                $scope.slide = 'slide-right';
+                $window.history.back();
+            }
+            $rootScope.go = function(path){
+                console.log(path);
+                $scope.slide = 'slide-left';
+                $location.url(path);
+            }
 
             $scope.timeFrame = "Today";
 
@@ -23,54 +23,64 @@ angular.module('peopleTracker.controllers', [])
                 $scope.timeFrame = newTimeFrame;
             };
 
-        PubNub.init({
+            PubNub.init({
                 publish_key   : 'pub-c-11781c51-4d7a-48ba-857f-2a6b5184ce10',
                 subscribe_key : 'sub-c-a709d546-f3cd-11e3-928e-02ee2ddab7fe'
             });
 
-        var publish = function() {
-            navigator.geolocation.watchPosition(geolocationSuccess, geolocationError);
-        };
+            var publish = function() {
+                navigator.geolocation.watchPosition(geolocationSuccess, geolocationError);
+            };
+            var geolocationSuccess = function(position){
+                testscope($rootScope, position);
 
-          var geolocationSuccess = function(position){
+            }
 
-              PubNub.ngPublish({
-                  channel: $scope.selectedEvent ,
-                  message: {
-                      "subscriber" : "subscriberA",
-                      "latitude": position.coords.latitude,
-                      "longitude": position.coords.longitude
-                  }
-              });
-      }
+            var testscope = function($rootScope, position)
+            {
+                PubNub.ngPublish({
+                    channel: $scope.selectedEvent ,
+                    message: {
+                        "subscriber" : $scope.userName,
+                        "latitude": position.coords.latitude,
+                        "longitude": position.coords.longitude
+                    }
+                });
+            }
 
-           $scope.subscribe = function(eventName) {
-          PubNub.ngSubscribe({ channel: eventName});
+            $scope.subscribe = function(eventName) {
+                PubNub.ngSubscribe({ channel: eventName});
 
-           $rootScope.$on(PubNub.ngMsgEv(eventName), function (event, payload) {
-            console.log(payload.message.subscriber + " : " +  payload.message.latitude + " : " + payload.message.longitude);
-          });
+                $rootScope.$on(PubNub.ngMsgEv(eventName), function (event, payload) {
+                    console.log(payload.message.subscriber + " : " +  payload.message.latitude + " : " + payload.message.longitude);
+                });
 
-           $scope.selectedEvent = eventName;
-           publish();
-           //  setInterval( $scope.publish(), 2000);
-           }
+                $scope.selectedEvent = eventName;
+                publish();
+                //  setInterval( $scope.publish(), 2000);
+            }
 
 
 
             var geolocationError = function(error){
                 console.log(error);
-          }
-                                       
-       
-         Events.get()
+            }
+
+
+            Events.get()
                 .success(function(data){
                     $scope.events =data;
                 })
-}])
-    .controller('MapCtrl', function($scope,$routeParams, PubNub) {
+        }])
+    .controller('MapCtrl','$scope','$routeParams','PubNub','$rootScope', [function($scope,$routeParams, PubNub,$rootScope) {
 
-        $scope.eventId = $routeParams.eventId;
+        $scope.lat = 46.87916;
+
+        $scope.markersMap = {};
+
+        $scope.mapPin = 'No pin set yet';
+
+        $scope.marker = { id: 1, lat: 20, long: 20 };
 
         $scope.onClick = function () {
 
@@ -95,10 +105,15 @@ angular.module('peopleTracker.controllers', [])
 
         var geolocationSuccess = function(position){
 
+            testscope($rootScope, position);
+        }
+
+        var testscope = function($rootScope, position)
+        {
             PubNub.ngPublish({
                 channel: $routeParams.eventId ,
                 message: {
-                    "subscriber" : "subscriberA",
+                    "subscriber" : $scope.userName,
                     "latitude": position.coords.latitude,
                     "longitude": position.coords.longitude
                 }
@@ -107,54 +122,23 @@ angular.module('peopleTracker.controllers', [])
         var geolocationError = function(error){
             console.log(error);
         }
-})
- .controller('AddPersonCtrl', function($scope, $routeParams, PubNub, $location) {
+    }])
+    .controller('AddPersonCtrl', ['$scope','$rootScope','PubNub','$location', function($scope, $rootScope,PubNub, $location) {
 
-       $scope.userName = "";
-       $scope.password = "";
+        $scope.userName = "";
+        $scope.password = "";
 
         var go = function(){
-            console.log('/events');
-            $scope.slide = 'slide-left';
-            $location.url('/events/test');
-        }
-
-
-        $scope.addPerson = function(){
-        go();
-
-}
-    }) //AddPersonCtrl
-.controller('AddEventCtrl', function($scope, $routeParams, PubNub, $location, Events) {
-
-       $scope.eventName = "";
-       $scope.address = "";
-       $scope.note = "";
-$scope.addEvent = function(){
-
-   
-  Events.post({ name: $scope.eventName, address: $scope.address, note: $scope.note });
-
-var go = function(){
-            console.log('/events');
             $scope.slide = 'slide-left';
             $location.url('/events');
         }
-   go();
-
-};
-}); //AddEventCtrl
 
 
+        $scope.addPerson = function(newUserName){
+            $rootScope.userName = newUserName;
+            go();
 
-
-
-
-
-
-
-
-
-
-
-
+        }
+    }]) //AddPersonCtrl
+    .controller('AddEventCtrl', function($scope, $routeParams, PubNub, $location) {
+    }); //AddEventCtrl
